598125a4d212c13560f2b833aca9192c9d47bbee
ZEBB <115534579+ZEBBN@users.noreply.github.com>
2024-05-06 18:18:19 +0200

    Oppdatert Filer
    Her har jeg lagt til andre filer og oppdatert eldre til å tilpasse dem til min liking.

--- notatapp/src/Components/Note.js
+++ notatapp/src/Components/Note.js
@@ -1,17 +1,19 @@
 import { MdDeleteForever } from 'react-icons/md';
 
-const Note = () => {
-    return(
-        <div className="note">
-            <span>TEST</span>
-            <div className="note-footer">
-                <small>03/05/2024</small>
-                <MdDeleteForever className='delete-icon' size='1.3em' />
-            </div>
-
-
-        </div>
-    )
+const Note = ({ id, text, date, handleDeleteNote }) => {
+	return (
+		<div className='note'>
+			<span>{text}</span>
+			<div className='note-footer'>
+				<small>{date}</small>
+				<MdDeleteForever
+					onClick={() => handleDeleteNote(id)}
+					className='delete-icon'
+					size='1.3em'
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Note;
