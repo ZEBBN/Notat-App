84c74d4646158c311a7a1599f554e53a05f477b4
ZEBB <115534579+ZEBBN@users.noreply.github.com>
2024-05-06 17:53:12 +0200

    Update App.js
    Skrevet inn flere av den samme koden for å få flere notes så det på appen ser det ut som at det er noe fremgang. Dette er kun midlertidig. Senere skal jeg legge til en funksjon der man kan legge til notater

--- notatapp/src/App.js
+++ notatapp/src/App.js
@@ -1,6 +1,32 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 import NotesList from "./Components/NotesList";
 
 const App = () => {
+	const [notes, setNotes] = useState([
+		{
+			id: nanoid(),
+			text: 'This is my first note!',
+			date: '15/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my second note!',
+			date: '21/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my third note!',
+			date: '28/04/2021',
+		},
+		{
+			id: nanoid(),
+			text: 'This is my new note!',
+			date: '30/04/2021',
+		},
+	]);
+
+
   return(<div className="container">
     <NotesList/>
   </div>)
